dd777d14faa5d831b34b16654b307a8fe0f5f0b0
Haseeb Qureshi <haseeb@haseebq.com>
2020-12-15 00:36:07 -0800

    Finish day15 part 2

--- day15.rb
+++ day15.rb
@@ -2,25 +2,34 @@ INPUT = '1,20,8,12,0,14'
 
 puts 'Part 1'
 
-sequence = INPUT.split(',').map(&:to_i)
-last_seen = Hash.new { |h, k| h[k] = [] }
+def nth_number(n)
+  sequence = INPUT.split(',').map(&:to_i)
+  last_seen = Hash.new { |h, k| h[k] = [] }
 
-0.upto(2019) do |i|
-  if sequence[i]
-    last_seen[sequence[i]] << i
-    next
+  0.upto(n - 1) do |i|
+    if sequence[i]
+      last_seen[sequence[i]] << i
+      next
+    end
+
+    if last_seen[sequence.last].length == 1
+      sequence << 0
+      sequence.shift
+      last_seen[0] << i
+    else
+      diff = last_seen[sequence.last].last(2).reduce(:-).abs
+      sequence << diff
+      sequence.shift
+      last_seen[diff] << i
+      last_seen[diff].shift if last_seen[diff].length > 2
+    end
   end
 
-  if last_seen[sequence.last].length == 1
-    sequence << 0
-    last_seen[0] << i
-  else
-    diff = last_seen[sequence.last].last(2).reduce(:-).abs
-    sequence << diff
-    last_seen[diff] << i
-  end
+  sequence.last
 end
 
-puts sequence.last
+puts nth_number(2020)
 
 puts 'Part 2'
+
+puts nth_number(30000000)
